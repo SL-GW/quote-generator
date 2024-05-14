12789ce2c84cd82836ff34b75d09ab8ebbd17a3b
SL-GW <162018972+SL-GW@users.noreply.github.com>
2024-05-14 12:27:09 +0200

    Added basic form handling and added typewriter plugin

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
+function generateQuote(event) {
+  event.preventDefault();
 
+  new Typewriter("#quote", {
+    strings: "Quote",
+    autoStart: true,
+    delay: 60,
+    cursor: "",
+  });
+}
+
+let quoteFormElement = document.querySelector("#quote-generator-form");
+quoteFormElement.addEventListener("submit", generateQuote);
